77a7d47574f29794f607fa9789d1f49c3665630d
Denise T <denise.turkova@justice.gov.uk>
2026-03-11 12:18:19 +0000

    feature n chore: added logout button and decreased ttl to 30 mins (#116)
    * feature n chore: added logout button and decreased ttl to 30 mins
    
    * chore formatting adjusted
    
    * updated package.json
    
    * updated dependencies
    
    * Fix and re-enable playwright tests in CI. Also revert Mocha to 11.7.5
    
    * Temporarily ignore npm dep issue
    
    * Change NODE_ENV to development to hopefully fix playwright tests
    
    * Update service name in playwright config
    
    * Fixes playwright tests by disabling auth for development environment
    
    * Fixed unit test issues
    
    ---------
    
    Co-authored-by: Tom Vaughan <tom.vaughan@justice.gov.uk>
    Co-authored-by: Lucas Main <lucas.e.main@gmail.com>

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -3,10 +3,10 @@ import { login, logout, redirect } from "#src/middleware/auth/auth-handlers.js";
 
 const authRouter = express.Router();
 
-authRouter.get("/login", login);
-
-authRouter.get("/redirect", redirect);
-
-authRouter.get("/logout", logout);
+if (process.env.NODE_ENV !== "development") {
+  authRouter.get("/login", login);
+  authRouter.get("/redirect", redirect);
+  authRouter.get("/logout", logout);
+}
 
 export default authRouter;
