1e6f9d498a54107681c1c8d26b9859c1a184308b
LucasM101 <104153582+LucasM101@users.noreply.github.com>
2026-03-16 14:16:28 +0000

    Enable Zap (#128)
    * Enable Zap
    
    * Enable Zap
    
    * Pin versions
    
    * Add Zap CI bits
    
    * update zap envs
    
    * add zap.yaml
    
    ---------
    
    Co-authored-by: Denise T <denise.turkova@justice.gov.uk>
    Co-authored-by: aloush-dev <github@aloush.dev>

---
+++ deploy/scripts/zap/zap-run-cicd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "--- Updating Zap.Yaml for CI --"
+sed -i 's|zap-results|tmp|g' zap.yaml
+
+docker compose \
+    -f $(pwd)/deploy/scripts/zap/docker-compose.yml \
+    up \
+    --exit-code-from zap-scan
