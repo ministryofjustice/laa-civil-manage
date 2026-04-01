6b2a1705019c41ea704c94bdeab5083b1dfbd4f0
Denise <denise.turkova@justice.gov.uk>
2026-04-01 15:10:44 +0100

    chore: fixed colours and formatting

--- src/controllers/WhatTypeOfPa.ts
+++ src/controllers/WhatTypeOfPa.ts
@@ -1,14 +1,13 @@
 import type { Request, Response, NextFunction } from "express";
 import router from "#src/routes/index.router.js";
 
-
 export const getWhatTypeOfPa = (
-  _req: Request, 
+  _req: Request,
   res: Response,
   next: NextFunction,
 ): void => {
   try {
-    res.render("main/what-type-of-pa"); 
+    res.render("main/what-type-of-pa");
   } catch (error) {
     next(error);
   }
