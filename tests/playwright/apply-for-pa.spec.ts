12fa15d2f0225697bf4a5874ad604578ff555a59
aloush-dev <github@aloush.dev>
2026-04-01 10:58:38 +0100

    add tests for the apply-for-pa page

---
+++ tests/playwright/apply-for-pa.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from "@playwright/test";
+
+test("page has correct title", async ({ page }) => {
+  await page.goto("/");
+
+  // CONTENTTODO
+  await expect(page).toHaveTitle(`Civil Manage – GOV.UK`);
+});
+
+test("page has heading with correct content", async ({ page }) => {
+  await page.goto("/");
+
+  // CONTENTTODO
+  const heading = page.getByRole("heading", {
+    name: "Apply for civil legal aid",
+  });
+
+  await expect(heading).toBeVisible();
+});
+
+test("page has a start button present and redirect to next page", async ({
+  page,
+}) => {
+  await page.goto("/");
+
+  const startButton = page.getByRole("button", {
+    name: "Start",
+  });
+
+  await expect(startButton).toBeVisible();
+
+  await startButton.click();
+
+  // CONTENTTODO
+  await expect(page).toHaveURL("/apply_sca_and_other");
+});
+
+// CONTENTTODO Add test to check for Codified rates and guideline hours link when we have one
