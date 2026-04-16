afe3b8e710354ec7c5af3da3f55eb851e638f6f3
Denise <denise.turkova@justice.gov.uk>
2026-04-16 11:55:29 +0100

    Added Playwright tests for confirmation page UI and navigation

---
+++ tests/playwright/pa-form/confirmation-page.spec.ts
@@ -0,0 +1,49 @@
+import { test, expect } from "@playwright/test";
+
+test("page has correct title", async ({ page }) => {
+  await page.goto("/pa-form/confirmation-page");
+
+  await expect(page).toHaveTitle(`Civil Manage – GOV.UK`);
+});
+
+test("page has heading with correct content", async ({ page }) => {
+  await page.goto("/pa-form/confirmation-page");
+
+  const heading = page.getByRole("heading", {
+    name: "Apply for civil legal aid",
+  });
+
+  await expect(heading).toBeVisible();
+});
+
+test("page has a Manage your application button present and redirect to placeholder page", async ({
+  page,
+}) => {
+  await page.goto("/pa-form/confirmation-page");
+
+  const startButton = page.getByRole("button", {
+    name: "Manage your application",
+  });
+
+  await expect(startButton).toBeVisible();
+  await startButton.click();
+
+  await expect(page).toHaveURL("/placeholder/mocked/stubbed");
+});
+
+test("page has a link taking to the feedback", async ({ page }) => {
+  await page.goto("/pa-form/confirmation-page");
+  const guidelineLink = page.getByRole("link", {
+    name: /Help us improve/i,
+  });
+
+  await expect(guidelineLink).toBeVisible();
+
+  const popupPromise = page.waitForEvent("popup");
+  await guidelineLink.click();
+  const newPage = await popupPromise;
+
+  await newPage.waitForLoadState();
+
+  await expect(newPage).toHaveURL(/.*feedback\/new/);
+});
