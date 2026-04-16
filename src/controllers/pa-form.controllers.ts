e42e1d90346f4ef8c7ec71ad08491f2d46899ae8
Denise <denise.turkova@justice.gov.uk>
2026-04-16 11:17:44 +0100

    Added pa-form controllers to improve maintainability

---
+++ src/controllers/pa-form.controllers.ts
@@ -0,0 +1,12 @@
+import type { Request, Response } from "#node_modules/@types/express/index.js";
+
+export const getStartingPage = (req: Request, res: Response): void => {
+  res.render("pa-form/start-page.njk");
+};
+export const getPaTypePage = (req: Request, res: Response): void => {
+  res.render("pa-form/type-pa.njk");
+};
+
+export const getConfirmationPage = (req: Request, res: Response): void => {
+  res.render("pa-form/confirmation-page");
+};
--- src/routes/pa-form.router.ts
+++ src/routes/pa-form.router.ts
@@ -1,23 +1,20 @@
 import express from "express";
-import type { Request, Response } from "#node_modules/@types/express/index.js";
+
+import {
+  getConfirmationPage,
+  getPaTypePage,
+  getStartingPage,
+} from "#src/controllers/pa-form.controllers.js";
 
 const paFormRouter = express.Router();
 
 // TODO This can be removed once the app has a landing page
-paFormRouter.get("/", (req: Request, res: Response) => {
-  res.render("pa-form/start-page.njk");
-});
+paFormRouter.get("/", getStartingPage);
 
-paFormRouter.get("/pa-form/start-page", (req: Request, res: Response) => {
-  res.render("pa-form/start-page.njk");
-});
+paFormRouter.get("/pa-form/start-page", getStartingPage);
 
-paFormRouter.get("/pa-form/type-pa", (req: Request, res: Response) => {
-  res.render("pa-form/type-pa.njk");
-});
+paFormRouter.get("/pa-form/type-pa", getPaTypePage);
 
-paFormRouter.get("/pa-form/confirmation-page", (req, res) => {
-  res.render("pa-form/confirmation-page");
-});
+paFormRouter.get("/pa-form/confirmation-page", getConfirmationPage);
 
 export default paFormRouter;
