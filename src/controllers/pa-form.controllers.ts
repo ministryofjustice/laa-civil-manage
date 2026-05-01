41ec143279591ef17e54a79c49f2e46d202441c9
Alan Sage <alan.sage@justice.gov.uk>
2026-05-01 14:25:18 +0100

    Add debug config and update PA field controller

--- src/controllers/pa-form.controllers.ts
+++ src/controllers/pa-form.controllers.ts
@@ -6,16 +6,25 @@ export const getStartPage = (req: Request, res: Response): void => {
   res.render("pa-form/start-page.njk");
 };
 
-
 export const getPaTypePage = (req: Request, res: Response): void => {
   res.render("pa-form/type-pa.njk");
 };
 
-export const postPaType = (req: Request<unknown,unknown, PriorAuthorityType>, res: Response): void => {
-  const priorAuthorityType = req.body;
+interface PaTypeBody {
+  WhatTypeOfPA: PriorAuthorityType;
+}
+
+export const postPaType = (
+  req: Request<unknown, unknown, PaTypeBody>,
+  res: Response,
+): void => {
+  const priorAuthorityType = req.body.WhatTypeOfPA;
   req.session.priorAuthority = { type: priorAuthorityType };
 
-  logger.logInfo("PriorAuthorityType", JSON.stringify(req.session.priorAuthority));
+  logger.logInfo(
+    "PriorAuthorityType",
+    JSON.stringify(req.session.priorAuthority),
+  );
 };
 
 export const getConfirmationPage = (req: Request, res: Response): void => {
