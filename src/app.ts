1ef7cbb8268674dc9c4a9c1aec7f255288e53054
Alan Sage <alan.sage@justice.gov.uk>
2026-05-05 10:50:16 +0100

    add csrf codeql exemption

--- src/app.ts
+++ src/app.ts
@@ -31,6 +31,9 @@ app.use(
 nunjucksSetup(app);
 setupMiddlewares(app);
 
+// csrf-sync isn't on the codeql whitelist here (https://github.com/github/codeql/blob/4c1461ad5bf31207de3b947a0864284e71c25e0c/javascript/ql/src/Security/CWE-352/MissingCsrfMiddleware.ql#L58).
+// It is, however, used in hmpps-template-typescript so we have confidence in this approach.
+// codeql[js/missing-token-validation]
 setupCsrf(app);
 
 app.set("trust proxy", 1);
