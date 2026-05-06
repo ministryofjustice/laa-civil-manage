cd6c162f5190f47984195d4da1ec48157f59c827
aloush-dev <github@aloush.dev>
2026-05-06 12:32:14 +0100

    add test for savetoSession

---
+++ tests/unit/middleware/saveToSessionMiddleware.spec.ts
@@ -0,0 +1,66 @@
+import { saveToSession } from "#src/middleware/saveToSession.js";
+import type { PriorAuthorityType } from "#src/types/prior-authority.js";
+import { describe, it, expect, mock } from "bun:test";
+import type { Request, Response, NextFunction } from "express";
+
+describe("saveToSession middleware", () => {
+  it("should extract the value, save it to the session, and call next()", () => {
+    const mockRequest = {
+      body: {
+        PriorAuthorityType: "Expert",
+      },
+      session: {},
+    } as unknown as Request<
+      unknown,
+      unknown,
+      { PriorAuthorityType: PriorAuthorityType }
+    >;
+
+    const mockResponse = {} as unknown as Response;
+
+    const mockNext = mock(() => {}) as unknown as NextFunction;
+
+    const middleware = saveToSession<
+      { PriorAuthorityType: PriorAuthorityType },
+      "type"
+    >("type", (body) => body.PriorAuthorityType);
+
+    middleware(mockRequest, mockResponse, mockNext);
+
+    expect(mockRequest.session.priorAuthority).toBeDefined();
+    expect(mockRequest.session.priorAuthority).toEqual({ type: "Expert" });
+
+    expect(mockNext).toHaveBeenCalled();
+  });
+
+  it("should merge with existing priorAuthority data without overwriting", () => {
+    const mockRequest = {
+      body: {
+        PriorAuthorityType: "Expense",
+      },
+      session: {
+        priorAuthority: {},
+      },
+    } as unknown as Request<
+      unknown,
+      unknown,
+      { PriorAuthorityType: PriorAuthorityType }
+    >;
+
+    const mockResponse = {} as unknown as Response;
+    const mockNext = mock(() => {}) as unknown as NextFunction;
+
+    const middleware = saveToSession<
+      { PriorAuthorityType: PriorAuthorityType },
+      "type"
+    >("type", (body) => body.PriorAuthorityType);
+
+    // 2. Act
+    middleware(mockRequest, mockResponse, mockNext);
+
+    expect(mockRequest.session.priorAuthority).toEqual({
+      type: "Expense",
+    });
+    expect(mockNext).toHaveBeenCalled();
+  });
+});
