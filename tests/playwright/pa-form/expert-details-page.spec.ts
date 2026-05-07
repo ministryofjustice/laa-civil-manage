5892be39bb48f1445f9726c9f9ce47fb99df959a
aloush-dev <github@aloush.dev>
2026-05-07 10:00:37 +0100

    update based on commends

--- tests/playwright/pa-form/expert-details-page.spec.ts
+++ tests/playwright/pa-form/expert-details-page.spec.ts
@@ -1,8 +1,10 @@
 import { test, expect } from "@playwright/test";
 
-test("page has a back link taking to the previous page", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("/pa-form/expert-details");
+});
 
+test("page has a back link taking to the previous page", async ({ page }) => {
   const backLink = page.getByRole("link", {
     name: "Back",
   });
@@ -15,8 +17,6 @@ test("page has a back link taking to the previous page", async ({ page }) => {
 });
 
 test("page has heading with correct content", async ({ page }) => {
-  await page.goto("/pa-form/expert-details");
-
   const heading = page.getByRole("heading", {
     name: "Expert details",
   });
@@ -27,8 +27,6 @@ test("page has heading with correct content", async ({ page }) => {
 test("page has input box for full name with correct label", async ({
   page,
 }) => {
-  await page.goto("/pa-form/expert-details");
-
   const fullNameInput = page.getByRole("textbox", {
     name: "Full Name",
   });
@@ -39,7 +37,6 @@ test("page has input box for full name with correct label", async ({
 test("when a user leaves the input empty and submits the form, an error is displayed", async ({
   page,
 }) => {
-  await page.goto("/pa-form/expert-details");
   await page.getByRole("button", { name: "Save and continue" }).click();
 
   const errorSummaryHeading = page.getByRole("heading", {
@@ -59,7 +56,6 @@ test("when a user leaves the input empty and submits the form, an error is displ
 test("when a user inputs a full name and submits the form they are redirected to the next page", async ({
   page,
 }) => {
-  await page.goto("/pa-form/expert-details");
   await page.getByRole("textbox", { name: "Full Name" }).fill("John Doe");
   await page.getByRole("button", { name: "Save and continue" }).click();
 
