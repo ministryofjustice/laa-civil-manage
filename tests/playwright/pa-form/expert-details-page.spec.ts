5dde27bc17798ca927c38af87a7d7475e91414ba
aloush-dev <github@aloush.dev>
2026-05-07 14:48:01 +0100

    add session test uncommented for when next page is done

--- tests/playwright/pa-form/expert-details-page.spec.ts
+++ tests/playwright/pa-form/expert-details-page.spec.ts
@@ -61,3 +61,23 @@ test("when a user inputs a full name and submits the form they are redirected to
 
   await expect(page).toHaveURL("/pa-form/document-upload");
 });
+
+//TODO - This test should be uncommented once the page after it has been implemented.
+
+// test("should persist the selected Prior Authority Expert Details when navigating back", async ({
+//   page,
+// }) => {
+//   const fullNameInput = page.getByRole("textbox", {
+//     name: "Full Name",
+//   });
+
+//   await fullNameInput.fill("John Doe");
+
+//   await page.getByRole("button", { name: "Save and continue" }).click();
+
+//   await expect(page).toHaveURL("/pa-form/confirmation-page");
+
+//   await page.getByRole("link", { name: "Back" }).click();
+
+//   await expect(fullNameInput).toHaveValue("John Doe");
+// });
