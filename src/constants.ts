0d00c9dce4dbecf18ccc15c3479c0939c3cfc2b3
aloush-dev <github@aloush.dev>
2026-05-07 15:19:16 +0100

    update pages

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,7 @@ export const pages = [
   "/",
   "/pa-form/start-page",
   "/pa-form/type-pa",
+  "/pa-form/expert-details",
+  "/pa-form/search-an-expert-type",
   "/pa-form/confirmation-page",
-  "/applications",
 ];
