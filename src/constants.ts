ba7b6e66c0dc2b48906a0ee1a2d9de815f4a7a88
jdwoolcock <30227772+jdwoolcock@users.noreply.github.com>
2026-05-08 09:22:37 +0100

    Add a11y checks to type of PA page (#205)
    * Add a11y checks to type of PA page
    
    * Fix formatting issues
    
    * add tests to template so they run on each page
    
    * update lock file
    
    * remove unneeded imports
    
    * update pages
    
    * update import
    
    ---------
    
    Co-authored-by: aloush-dev <github@aloush.dev>
    Co-authored-by: Ali <81445071+aloush-dev@users.noreply.github.com>

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const pages = [
+  "/",
+  "/pa-form/start-page",
+  "/pa-form/type-pa",
+  "/pa-form/expert-details",
+  "/pa-form/search-an-expert-type",
+  "/pa-form/confirmation-page",
+];
