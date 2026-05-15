5b320538d7459d69c8ae55a76947cf9773437831
aloush-dev <github@aloush.dev>
2026-05-15 16:07:58 +0100

    linting update

--- src/utils/documentUploadHelpers.ts
+++ src/utils/documentUploadHelpers.ts
@@ -1,11 +1,12 @@
 import type { Request } from "#node_modules/@types/express";
-import type { PriorAuthority, UploadedDocument } from "#src/types/prior-authority.js";
+import type {
+  PriorAuthority,
+  UploadedDocument,
+} from "#src/types/prior-authority.js";
 
 export const FILE_SIZE_ERROR = "The selected file must be smaller than 7MB";
 
-export const buildUploadedFilesList = (
-  docs: UploadedDocument[],
-): object[] =>
+export const buildUploadedFilesList = (docs: UploadedDocument[]): object[] =>
   docs.map((doc) => ({
     message: { text: doc.originalFileName },
     fileName: doc.fileName,
