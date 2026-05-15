8ef4c058c3e525cbdbf0a2ff9a9ea144c26a3860
aloush-dev <github@aloush.dev>
2026-05-15 16:00:04 +0100

    refix typo

--- src/routes/document-upload.router.ts
+++ src/routes/document-upload.router.ts
@@ -159,7 +159,7 @@ documentUploadRouter.post(
     if (file === undefined) {
       return res.status(400).json({ error: { message: "No file received" } });
     }
-    const { originalName } = file;
+    const { originalname } = file;
     const fileName = randomUUID();
     const doc: UploadedDocument = { fileName, originalFileName: originalname };
     const priorAuthority: Partial<PriorAuthority> =
