5ac3bebdca410dd464ad07927627c8ac1f920c94
LucasM101 <104153582+LucasM101@users.noreply.github.com>
2026-05-19 17:16:45 +0100

    Ap 6964 document upload review comments (#254)
    * Style changes to address review comments and add File name and Action headings
    
    * Update Uploaded Files title
    
    * add pages to a11y test

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -9,6 +9,18 @@ if ($multiFileUpload !== null) {
     .querySelector('meta[name="_csrf"]')
     ?.getAttribute("content");
 
+  const $list = $multiFileUpload.querySelector(".moj-multi-file-upload__list");
+  if ($list !== null) {
+    const $header = document.createElement("div");
+    $header.className =
+      "govuk-summary-list__row moj-multi-file-upload__row moj-multi-file-upload__header-row";
+    $header.innerHTML = `
+      <div class="govuk-summary-list__value"><strong>File name</strong></div>
+      <div class="govuk-summary-list__actions"><strong>Action</strong></div>
+    `;
+    $list.insertBefore($header, $list.firstChild);
+  }
+
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call, no-new -- MultiFileUpload is a third-party JS module without TypeScript declarations, instantiated for side effects
   new MultiFileUpload($multiFileUpload, {
     uploadUrl: `/ajax-upload-url?_csrf=${csrfToken}`,
