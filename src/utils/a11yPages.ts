70fd3446421f91058f5a8aacd65a26c2813e94a1
Alan Sage <alan.sage@justice.gov.uk>
2026-05-21 15:11:23 +0100

    remove unused code

--- src/utils/a11yPages.ts
+++
@@ -1,6 +0,0 @@
-import { documentUploadA11yPages } from "#src/routes/document-upload.router.js";
-import { paFormA11yPages } from "#src/routes/pa-form.router.js";
-
-export const a11yPages = [
-  ...new Set([...paFormA11yPages, ...documentUploadA11yPages]),
-].sort();
