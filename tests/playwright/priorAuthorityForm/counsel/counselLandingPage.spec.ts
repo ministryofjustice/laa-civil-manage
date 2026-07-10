93800c6e5ffd74e79f7a2c6294b6015dbaf69810
Lucas Main <lucas.e.main@gmail.com>
2026-07-10 09:53:51 +0100

    Tidy up

--- tests/playwright/priorAuthorityForm/counsel/counselLandingPage.spec.ts
+++ tests/playwright/priorAuthorityForm/counsel/counselLandingPage.spec.ts
@@ -33,7 +33,7 @@ test.describe("Counsel page", () => {
 
     await startButton.click();
 
-    await expect(page).toHaveURL("/");
+    await expect(page).toHaveURL("/counsel/type-of-counsel");
   });
 
   test("page has a back link taking to the previous page", async ({ page }) => {
