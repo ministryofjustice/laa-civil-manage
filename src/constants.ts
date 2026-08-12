b80f88d3c03a4f957be99b8347146722f4f8713a
Lucas Main <lucas.e.main@gmail.com>
2026-08-12 09:08:53 +0100

    remove temp postcode

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,6 @@
 // TODO Temporary hardcoded application ID used until the parent application flow is in scope.
 export const DEV_APPLICATION_ID = "00000000-0000-0000-0000-000000000001";
 
-// TODO Temporary fallback values until dedicated frontend pages are wired.
-export const TEMP_EXPERT_POSTCODE = "SW1H 9AJ" as string;
-
 export const pages = [
   // General
   "/",
